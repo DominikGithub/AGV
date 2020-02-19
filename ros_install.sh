b4b6d6caca2d535fde2f6f6406c9abf42047912a
Dominik <dominik.irimi@gmail.com>
2020-02-19 20:17:37 +0100

    ydliar rm submodule

--- ros_install.sh
+++ ros_install.sh
@@ -1,5 +1,7 @@
-apt-get update && 
-apt-get upgrade && 
-apt-get install ros-melodic-navigation \
+#apt-get update && 
+#apt-get upgrade  -y && 
+apt-get install -y ros-melodic-navigation \
                 ros-melodic-geometry \
-                ros-melodic-hector-mapping
+                ros-melodic-hector-mapping \
+		ros-melodic-teleop-twist-keyboard \
+		ros-melodic-hector-slam
