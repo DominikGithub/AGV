6d246873f070d79f2df1bdcd8749b51ab687b90b
Dominik Irimi <dominik.irimi@protonmail.ch>
2020-07-19 13:07:35 +0000

    init parallel motor driver

--- src/motor_pkg/src/motor_ctrl/motor.py
+++ src/motor_pkg/src/motor_ctrl/motor.py
@@ -1,13 +1,14 @@
 #! /usr/bin/env python
 
 import os
+from multiprocessing import Process, Pipe
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 import RPi.GPIO as GPIO
 import time
 
-def twist2velocity(twist):
+def twist2velocity(twist, ros_write_conn):
     """
     Convert twist to motor velocities.
     @param twist: ROS topic twist (linear, angular)
@@ -23,83 +24,117 @@ def twist2velocity(twist):
         speed_left = twist.linear.x - speed_angular
         speed_right = twist.linear.x + speed_angular
   
-    setSpeed(speed_left, speed_right)
+    ros_write_conn.send([speed_left, speed_right])
+    #setSpeed(speed_left, speed_right)
 
-def constrain(value, low, high):
-    """
-    Limit max velocity.
-    @param value: actual speed
-    @param low: max negative speed limit
-    @param high: max positive speed limit
-    @returns: constrained speed value
-    """
-    if value < low: return low
-    if value > high: return high
-    return value
-
-def setSpeed(s_left, s_right):
+#def setSpeed(s_left, s_right):
+def setSpeed(motor_read_conn):
     """
     Actuate differential drive motors.
     @param s_left: speed left motor
     @param s_right: speed right motor
     """
 
-    rospy.loginfo("TWIST SPEED: %s, %s\r" % (s_left, s_right))
+    while True:
+        s_left, s_right = motor_read_conn.recv()
 
-    s_left  = s_left * 100
-    s_right = s_right * 100
+        rospy.loginfo("TWIST SPEED: %s, %s\r" % (s_left, s_right))
 
-    # motor
-    try:
-        # motor pins
-        left_forw = 12
-        left_back = 32
-        right_forw = 33
-        right_back = 35
+        s_left  = s_left * 100
+        s_right = s_right * 100
 
-        if s_left == 0:  pass
-        elif s_left > 0: l_pin = left_forw
-        else:            l_pin = left_back
+        # motor
+        try:
+            # motor pins
+            left_forw = 12
+            left_back = 32
+            right_forw = 33
+            right_back = 35
 
-        if  s_right == 0: pass
-        elif s_right > 0: r_pin = right_forw
-        else:             r_pin = right_back
-        
-        # GPIO setup
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BOARD)
-        if s_left != 0:GPIO.setup(l_pin, GPIO.OUT)
-        if s_right != 0:GPIO.setup(r_pin, GPIO.OUT)
+            if s_left == 0:  pass
+            elif s_left > 0: l_pin = left_forw
+            else:            l_pin = left_back
 
-        if s_left != 0: l_pwm = GPIO.PWM(l_pin, 10)
-        if s_right != 0:r_pwm = GPIO.PWM(r_pin, 10)
+            if  s_right == 0: pass
+            elif s_right > 0: r_pin = right_forw
+            else:             r_pin = right_back
+            
+            # GPIO setup
+            GPIO.setwarnings(False)
+            GPIO.setmode(GPIO.BOARD)
+            if s_left != 0:GPIO.setup(l_pin, GPIO.OUT)
+            if s_right != 0:GPIO.setup(r_pin, GPIO.OUT)
 
-        # actuate
-        if s_left != 0: l_pwm.start(abs(s_left) if abs(s_right) < 100 else 100)
-        if s_right != 0:r_pwm.start(abs(s_right) if abs(s_right) < 100 else 100)
-        time.sleep(1)
+            if s_left != 0: l_pwm = GPIO.PWM(l_pin, 100)
+            if s_right != 0:r_pwm = GPIO.PWM(r_pin, 100)
 
-        # stop
-        if s_left != 0: l_pwm.stop()
-        if s_right != 0:r_pwm.stop()
-    except Exception as ex:
-        rospy.logerr("%s\r" % ex)
-    finally:
-        # reset pin in/out state
-        GPIO.cleanup()
+            # actuate
+            if s_left != 0: l_pwm.start(abs(s_left) if abs(s_right) < 100 else 100)
+            if s_right != 0:r_pwm.start(abs(s_right) if abs(s_right) < 100 else 100)
+            time.sleep(1)
 
-def callback(twist):
+            # stop
+            if s_left != 0: l_pwm.stop()
+            if s_right != 0:r_pwm.stop()
+        except Exception as ex:
+            rospy.logerr("%s\r" % ex)
+        finally:
+            # reset pin in/out state
+            GPIO.cleanup()
+    
+        # yield thread
+        time.sleep(0)
+
+def callback(twist, args):
     """
     Ros topic cmd_vel subscription callback.
     @param twist: ROS topic twist
     """
+    
+    # get threadind pipe connection
+    ros_write_conn = args[0]
+
     log_txt = "%s received -> Linear: %s Angular: %s\r" % (rospy.get_caller_id(), twist.linear, twist.angular)
     log_txt = log_txt.replace('\n', ' ')
     rospy.loginfo(log_txt, logger_name="motor_node_logger")
 
     # convert twist to motor velocity
-    twist2velocity(twist)
+    twist2velocity(twist, ros_write_conn)
 
+
+
+
+def prod_driver(write_conn):
+    counter = 0
+    while True:
+        write_conn.send(['counter', counter])
+        counter = counter + 1
+        time.sleep(0)
+
+def motor_driver(read_conn):
+    """
+    Execute motor command.
+    @param read_conn: python multithreading pipe connection object.
+    """
+    while True:
+        print("\033[1;34m %s \033[0m\r" % read_conn.recv())
+        time.sleep(0)
+
+"""
+def init_motor_process():
+    motor_read_conn, ros_write_conn = Pipe()
+    pm = Process(target=motor_driver, args=(motor_read_conn,))
+    pr = Process(target=prod_driver, args=(ros_write_conn,))
+    
+    ros_write_conn.send([42, None, 'hello'])
+    #motor_read_conn.close()
+    
+    pm.start()
+    pr.start()
+    
+    #pm.join()
+    return pm, pr
+"""
 
 def listen_cmd():
     """
@@ -110,10 +145,19 @@ def listen_cmd():
         ascii_art_str = file.read()
         print("\033[1;34m" + ascii_art_str + "\033[0m")
 
+    #pm, pr = init_motor_process()
+    motor_read_conn, ros_write_conn = Pipe()
+    pm = Process(target=setSpeed, args=(motor_read_conn,))
+    pm.start()
+
     # init node
     rospy.init_node('motor_node', anonymous=False)
-    rospy.Subscriber("cmd_vel", Twist, callback)
+    rospy.Subscriber("cmd_vel", Twist, callback, (ros_write_conn,))
     rospy.spin()
+
+    # join worker threads
+    pm.join()
+    #pr.join()
 
 if __name__ == '__main__':
     listen_cmd()
